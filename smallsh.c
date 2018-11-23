4c43734cce3aa778958fe6c4fd8a0b2393875c42
hyeonson <gumgim95@naver.com>
2018-11-23 10:48:31 +0900

    2018 1123 1048  쉘 구현2

--- smallsh.c
+++ smallsh.c
@@ -10,56 +10,44 @@
 
 #define MAX_CMD_ARG 10
 #define MAX_CMD_GRP 10
-// #define BUF_SIZE 512
-
-const char *prompt = "myshell> ";
+const char* prompt = "myshell> ";
 
 struct sigaction act;
 
 char* cmdgrps[MAX_CMD_GRP];
 char* cmdvector[MAX_CMD_ARG];
 char cmdline[BUFSIZ];
+
 void fatal(char* str);
 void execute_cmdline(char* cmdline);
 void execute_cmdgrp(char* cmdgrp);
-//int makelist(char* s, const char* delimiters, char** list, int MAX_LIST);
-int makeargv(char* s, const char* delimiters, char** list, int MAX_LIST);
+int makelist(char* s, const char* delimiters, char** list, int MAX_LIST);
 int isBackground(char* s);
 int cmd_exit(int argc, char* argv[]);
 int cmd_cd(int argc, char* argv[]);
-void read_childproc(int sig);
+void zombie_handler(int sig);
 
 int main(int argc, char **argv)
 {
     int i = 0;
-    //pid_t pid;
-
+    
     act.sa_flags = SA_RESTART; //시그널 핸들러에 의해 중지된 시스템 호출을 자동적으로 재시작한다.
-    //act.sa_flags = 0;
 	sigemptyset(&act.sa_mask);
-	act.sa_handler = read_childproc;
+	act.sa_handler = zombie_handler;
 	sigaction(SIGCHLD, &act, 0);
-
+    
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+    signal(SIGTTOU, SIG_IGN);
+    
+    
     while (1)
     {
         fputs(prompt, stdout);
         fgets(cmdline, BUFSIZ, stdin);
         cmdline[strlen(cmdline) - 1] = '\0';
-        /*
-        switch (pid = fork())
-        {
-        case 0:
-            makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
-            execvp(cmdvector[0], cmdvector);
-            fatal("main()");
-        case -1:
-            fatal("main()");
-        default:
-            wait(NULL);
-        }
-        */
-       execute_cmdline(cmdline);
-       printf("\n");
+        
+        execute_cmdline(cmdline);
     }
     return 0;
 }
@@ -67,9 +55,9 @@ int main(int argc, char **argv)
 void fatal(char *str)
 {
     perror(str);
-    exit(1);
+    return;
 }
-/*
+
 int makelist(char *s, const char *delimiters, char **list, int MAX_LIST)
 {
     int i = 0;
@@ -80,32 +68,6 @@ int makelist(char *s, const char *delimiters, char **list, int MAX_LIST)
         return -1;
 
     snew = s + strspn(s, delimiters); // delimiters를 skip 
-    if ((list[numtokens] = strtok(snew, delimiters)) == NULL)
-        return numtokens;
-
-    numtokens = 1;
-
-    while (1)
-    {
-        if ((list[numtokens] = strtok(NULL, delimiters)) == NULL)
-            break;
-        if (numtokens == (MAX_LIST - 1))
-            return -1;
-        numtokens++;
-    }
-    return numtokens;
-}
-*/
-int makeargv(char* s, const char* delimiters, char** list, int MAX_LIST)
-{
-    int i = 0;
-    int numtokens = 0;
-    char *snew = NULL;
-
-    if ((s == NULL) || (delimiters == NULL))
-        return -1;
-
-    snew = s + strspn(s, delimiters); /* delimiters를 skip */
     if ((list[numtokens] = strtok(snew, delimiters)) == NULL)
         return numtokens;
 
@@ -131,13 +93,14 @@ void execute_cmdline(char* cmdline)
     int pid;
     char cmdgrpstemp[BUFSIZ];
     int background;
-    count = makeargv(cmdline, ";", cmdgrps, MAX_CMD_GRP);//makelist
+    count = makelist(cmdline, ";", cmdgrps, MAX_CMD_GRP);
     for(i = 0; i < count; i++)
     {
-        //makeargv때문에 바뀌는 cmdgrps[i]를 위해 원본은 유지한다 + 1 은 널 때문에
+        //makelist때문에 바뀌는 cmdgrps[i]를 위해 원본은 유지한다 + 1 은 널 때문이다
         memcpy(cmdgrpstemp, cmdgrps[i], strlen(cmdgrps[i]) + 1);
         background = isBackground(cmdgrpstemp);
-        arg_count = makeargv(cmdgrpstemp, " \t", cmdvector, MAX_CMD_ARG);
+
+        arg_count = makelist(cmdgrpstemp, " \t", cmdvector, MAX_CMD_ARG);
         if(strcmp(cmdvector[0], "cd") == 0)
         {
             cmd_cd(arg_count, cmdvector);
@@ -148,16 +111,19 @@ void execute_cmdline(char* cmdline)
             cmd_exit(arg_count, cmdvector);
             return;
         }
-        
         switch(pid = fork())
         {
-            case -1: fatal("fork error");
+            case -1:
+                fatal("fork error");
+                break;
             case 0:
-                execute_cmdgrp(cmdgrps[i]);//execute_cmdgrp(cmdgrpstemp) 대신
+                execute_cmdgrp(cmdgrps[i]);//cmdgrpstemp 대신 원본을 쓴다
             default:
                 if(background == 0)
+                {
                     waitpid(pid, NULL, 0);
-                    //break;
+                    tcsetpgrp(STDIN_FILENO, getpgid(0));
+                }
                 fflush(stdout);
         }
     }
@@ -174,7 +140,6 @@ int isBackground(char* s)
             return 1;
         }
     }
-    
     return 0;
 }
 
@@ -184,9 +149,14 @@ void execute_cmdgrp(char* cmdgrp)
     int count = 0;
     int background;
 
-    background = isBackground(cmdgrp);
+    signal(SIGINT, SIG_DFL);
+    setpgid(0, 0);
 
-    count = makeargv(cmdgrp, " \t", cmdvector, MAX_CMD_ARG);
+    background = isBackground(cmdgrp);
+    if(background == 0)
+        tcsetpgrp(STDIN_FILENO, getpgid(0));
+    
+    count = makelist(cmdgrp, " \t", cmdvector, MAX_CMD_ARG);
     execvp(cmdvector[0], cmdvector);
     fatal("exec error");
 }
@@ -199,7 +169,7 @@ int cmd_cd(int argc, char* argv[])
     {
         if(strcmp(argv[1], "~") == 0)
             chdir(getenv("HOME"));
-        else if(chdir(argv[1]) == -1)
+        else if(chdir(argv[1]) == 1)
             fatal("chdir error");
     }
     else
@@ -210,27 +180,21 @@ int cmd_cd(int argc, char* argv[])
 
 int cmd_exit(int argc, char* argv[])
 {
-    printf("exit!\n");
+    printf("my shell exit!\n");
     exit(0);
 
     return 0;
 }
 
-void read_childproc(int sig)
+void zombie_handler(int sig)
 {
     int status;
     int pid = waitpid(-1, &status, WNOHANG);
-
-    /*
-    while ((pid = waitpid(-1, &stat, WNOHANG)) > 0)
-        printf("child %d terminated normaly\n", pid) ;
-    */
 
     if(pid > 0)
     {
         printf("background process child %d terminated normaly\n", pid) ;
         fputs(prompt, stdout);
         fflush(stdout);
-        //printf("%s", prompt);
     }
 }
